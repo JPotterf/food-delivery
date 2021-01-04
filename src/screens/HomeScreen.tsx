73435ca7e5cecd18030422b5756044d9f3609966
JPotterf <Jacob.Potterf@gmail.com>
2021-01-04 20:15:55 +0200

    useEffect hook to reverse geocode address. Built out main nav routes.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -11,6 +11,7 @@ export const HomeScreen = () => {
                 <Text>Navigation </Text> 
             </View>
             <View style={styles.body}>
+             
                 <Text> Home Screen </Text>
             </View>
             <View style={styles.footer}>
@@ -26,21 +27,18 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        backgroundColor: 'green'
+        backgroundColor: 'rgba(242,242,242,1)'
     },
     navigation: {
         flex: 2,
-        backgroundColor: 'red'
     },
     body: {
         flex: 9,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'yellow'
     },
     footer: {
         flex: 1,
-        backgroundColor: 'cyan'
     }
 
 })
--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,20 +1,57 @@
-import React from 'react'
+import React,{useState, useReducer, useEffect} from 'react'
 import { View, Text, StyleSheet, Dimensions , Image } from 'react-native'
+import * as Location from 'expo-location'
 
-
+const screenWidth = Dimensions.get('screen').width;
 
 export const LandingScreen = () => {
     
+    const [errorMsg, setErrorMsg] = useState("");
+    const [address, setAddress] = useState<Location.Address>();
+
+    const [displayAddress, setDisplayAddress] = useState("Waiting for Current Address");
+
+    useEffect(()=> {
+        (async()=> {
+            let {status} = await Location.requestPermissionsAsync();
+            if(status !== 'granted'){
+                setErrorMsg('Permission to access location not granted')
+            }
+        
+            let location: any = await Location.getCurrentPositionAsync({});
+            const {coords} = location;
+
+            if(coords){
+                const{latitude, longitude} = coords;
+                let addressResponse: any = await Location.reverseGeocodeAsync({latitude,longitude})
+
+                for(let item of addressResponse){
+                    setAddress(item)
+                    let currentAddress = `$(item.name), $(item.street), $(item.postalCode), $(item.country)`
+                    setDisplayAddress(currentAddress);
+                }
+            }else{
+                //notify user of error
+            }
+        })
+
+    },[])
+
     return (
         <View style={styles.container}>
             <View style={styles.navigation}> 
-                <Text>Navigation </Text> 
+                <Text> </Text> 
             </View>
             <View style={styles.body}>
-                <Text> Landing Screen </Text>
+            <Image source={require('../images/delivery_icon.png')} style={styles.deliveryIcon}/>
+                <View style={styles.addressContainer}>
+                    <Text style={styles.addressTitle}> Enter Your Delivery Address </Text>
+                </View>
+
+                <Text style={styles.addressText}>{displayAddress}</Text>
             </View>
             <View style={styles.footer}>
-                <Text> Footer </Text>
+                <Text>  </Text>
             </View>
         </View>
     )
@@ -26,21 +63,40 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        backgroundColor: 'green'
+        backgroundColor:'rgba(242,242,242,1)'
     },
     navigation: {
         flex: 2,
-        backgroundColor: 'red'
     },
     body: {
         flex: 9,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'yellow'
+    },
+    deliveryIcon:{
+        width:120,
+        height:120
+    },
+    addressContainer:{
+        width:screenWidth - 100,
+        borderBottomColor: 'red',
+        borderBottomWidth: 0.5,
+        padding: 5,
+        marginBottom: 10,
+        alignItems: 'center'        
+    },
+    addressTitle:{
+        fontSize: 22,
+        fontWeight: '700',
+        color: '#7D7D7D',
+        marginTop: 10,
+    },
+    addressText:{
+        fontSize: 18,
+        fontWeight: '200',
+        color: '#4F4F4F'
     },
     footer: {
         flex: 1,
-        backgroundColor: 'cyan'
     }
-
 })
